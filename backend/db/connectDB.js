cc2448dd51ed8408cf75743d02befc3bd9a179ae
aditya6273 <adityashah2701@gmail.com>
2024-09-10 11:40:22 +0530

    db url

--- backend/db/connectDB.js
+++ backend/db/connectDB.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
 	try {
-		const conn = await mongoose.connect(process.env.MONGO_URI, {
+		const conn = await mongoose.connect(process.env.MONGO_URI_LIVE, {
 			// To avoid warnings in the console
 			useNewUrlParser: true,
 			useUnifiedTopology: true,
